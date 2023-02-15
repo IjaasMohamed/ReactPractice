c6159edbd9e04ac31595cfd9220e273c80435cff
IjaasMohamed <undyin0@gmail.com>
2023-02-15 04:41:13 -0300

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
     <div className="container">
-      <h1>Learn React</h1>
-      <form>
-        <div className="mb-3">
-          <input type="text" className="form-control" />
+      <h1 className="display-4">Learn React</h1>
+      <br />
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <form>
+            <div className="mb-3">
+              <input type="text" className="form-control" />
+            </div>
+            <div className="mb-3">
+              <input type="password" className="form-control" />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
         </div>
-        <div className="mb-3">
-          <input type="password" className="form-control" />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
