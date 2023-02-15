69abe79896a29764d5de5600408c54838e8b717d
IjaasMohamed <undyin0@gmail.com>
2023-02-15 12:21:18 -0300

    33

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
+import { useState } from "react";
+
 function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("State => ", email, password);
+  };
+
   return (
     <div className="container">
       <h1 className="display-4">Learn React</h1>
       <br />
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
-          <form>
-            <div className="mb-3">
-              <input type="text" className="form-control" />
-            </div>
-            <div className="mb-3">
-              <input type="password" className="form-control" />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className="form-label">Email address</label>
+          <input
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            type="email"
+            className="form-control"
+          />
+          <div className="form-text">
+            We'll never share your email with anyone else.
+          </div>
         </div>
-      </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            type="password"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3 form-check">
+          <input type="checkbox" className="form-check-input" />
+          <label className="form-check-label">Check me out</label>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
