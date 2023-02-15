a37e1116b0a437bf195338488374ea3c1636ba3f
IjaasMohamed <undyin0@gmail.com>
2023-02-15 03:49:18 -0300

    02

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Learn React</h1>
+      <form>
+        <div className="mb-3">
+          <input type="text" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <input type="password" className="form-control" />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
